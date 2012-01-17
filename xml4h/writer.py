d1e8941739aedbaa278771991597b7b1173c9566
James Murty <james@murty.co>
2012-01-18 10:16:00 +1100

    Filling out XML writer

--- xml4h/writer.py
+++ xml4h/writer.py
@@ -25,7 +25,6 @@ def _sanitize_write_params(indent='', newline=''):
 
     return (indent, newline)
 
-# TODO: Avoid relying on wrapped version of every node, too inefficient?
 def write(node, writer, encoding='utf-8',
         indent=0, newline='', quote_char='"', omit_declaration=False,
         _depth=0):
@@ -35,20 +34,40 @@ def write(node, writer, encoding='utf-8',
     # Output document declaration if we're outputting the whole doc
     if node.is_document:
         if not omit_declaration:
-            writer.write('<?xml version="1.0"')
+            writer.write('<?xml version=%s1.0%s' % (quote_char, quote_char))
             if encoding:
-                writer.write(' encoding="%s"' % encoding)
+                writer.write(' encoding=%s%s%s'
+                    % (quote_char, encoding, quote_char))
             writer.write('?>%s' % newline)
         for child in node.children:
             write(child, writer, encoding, indent, newline, quote_char,
                     omit_declaration, _depth)  # Note _depth not incremented
+    elif node.is_document_type:
+        writer.write("<!DOCTYPE %s SYSTEM %s%s%s"
+            % (node.name, quote_char, node.public_id))
+        if node.system_id is not None:
+            writer.write(" %s%s%s" % (quote_char, node.system_id, quote_char))
+        if node.children:
+            writer.write("[")
+            for child in node.children:
+                write(child, writer, encoding=encoding,
+                    indent=indent, newline=newline, quote_char=quote_char,
+                    omit_declaration=omit_declaration, _depth=_depth+1)
+            writer.write("]")
+        writer.write(">")
     elif node.is_text:
         writer.write(_sanitize_write_value(node.value))
     elif node.is_cdata:
         if ']]>' in node.value:
             raise Exception("']]>' is not allowed in CDATA node value")
         writer.write("<![CDATA[%s]]>" % node.value)
-    #elif node.is_entity: # TODO
+    #elif node.is_entity_reference:  # TODO
+    elif node.is_entity:
+        writer.write("<!ENTITY ")
+        if node.is_paremeter_entity:
+            writer.write('%% ')
+        writer.write("%s %s%s%s>"
+            % (node.name, quote_char, node.value, quote_char))
     elif node.is_processing_instruction:
         writer.write(indent)
         writer.write("<?%s %s?>" % (node.target, node.data))
@@ -57,16 +76,27 @@ def write(node, writer, encoding='utf-8',
         if '--' in node.value:
             raise Exception("'--' is not allowed in COMMENT node value")
         writer.write("<!--%s-->" % node.value)
-    #elif node.is_notation: # TODO
+    elif node.is_notation:
+        writer.write("<!NOTATION %s" % node.name)
+        if node.is_system_identifier:
+            writer.write(" system %s%s%s>"
+                % (quote_char, node.external_id, quote_char))
+        elif node.is_system_identifier:
+            writer.write(" system %s%s%s %s%s%s>"
+                % (quote_char, node.external_id, quote_char,
+                   quote_char, node.uri, quote_char))
+    elif node.is_attribute:
+        writer.write(" %s=%s" % (node.name, quote_char))
+        writer.write(_sanitize_write_value(node.value))
+        writer.write(quote_char)
     elif node.is_element:
         writer.write(indent * _depth)
         writer.write("<" + node.name)
 
         for attr in node.attribute_nodes:
-            writer.write(" %s=%s" % (attr.name, quote_char))
-            # TODO Handle CDATA nodes (don't munge data)
-            writer.write(_sanitize_write_value(attr.value))
-            writer.write(quote_char)
+            write(attr, writer, encoding=encoding,
+                indent=indent, newline=newline, quote_char=quote_char,
+                omit_declaration=omit_declaration, _depth=_depth)
         if node.children:
             found_indented_child = False
             last_child_was_indented = False
