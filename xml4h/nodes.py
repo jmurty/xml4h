c20d6734aee8090789d1ca876ff81ccf273cce62
James Murty <james@murty.co>
2012-09-11 23:54:43 +1000

    Make Document/Element attribute values available via index key lookup

--- xml4h/nodes.py
+++ xml4h/nodes.py
@@ -284,20 +284,34 @@ class Node(object):
             omit_declaration=omit_declaration, _depth=_depth)
 
 
-class _NodeWithElementChildrenMixin(object):
+class _MagicNodeAttrItemLookupsMixin(object):
+
+    def __getitem__(self, attr_name):
+        """
+        Magical retrieval of this XML element's attribute values.
+        """
+        result = self.attributes[attr_name]
+        if result is None:
+            raise IndexError()
+        else:
+            return result
 
     def __getattr__(self, child_name):
         """
-        Magical traversal of child XML elements by name.
+        Magical traversal of child XML elements by name when name contains
+        an uppercase character, or ends with an underscore (for lowercase
+        element names).
         """
-        # TODO This feels too broad to be safe, should be more restrictive?
-        if child_name and child_name[0] != '_':
+        if child_name.startswith('_'):
+            # Never match names with underscore leaders, for safety
+            pass
+        elif child_name != child_name.lower() or child_name.endswith('_'):
+            # Names with uppercase characters or trailing '_' are fair game
             results = self.get(child_name)
             if len(results) == 1:
                 return results[0]
             elif len(results) > 1:
                 return results
-        # TODO: Localize exception message
         raise AttributeError(
             "'%s' object has no attribute '%s'"
             % (self.__class__.__name__, child_name))
@@ -319,7 +333,7 @@ class _NodeWithElementChildrenMixin(object):
         return self.get(child_name, first_only=True)
 
 
-class Document(Node, _NodeWithElementChildrenMixin):
+class Document(Node, _MagicNodeAttrItemLookupsMixin):
     _node_type = DOCUMENT_NODE
     # TODO: doc_type, document_element
 
@@ -423,7 +437,7 @@ class ProcessingInstruction(_NameValueNode):
     data = _NameValueNode.value
 
 
-class Element(_NameValueNode, _NodeWithElementChildrenMixin):
+class Element(_NameValueNode, _MagicNodeAttrItemLookupsMixin):
     """
     Wrap underlying XML document-building libarary/implementation and
     expose utility functions to easily build XML nodes.
