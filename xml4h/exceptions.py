939e5721300013b2977375f28897a6a573509112
James Murty <james@murty.co>
2012-10-03 23:37:12 +1000

    Rename base exception class; less ugly

--- xml4h/exceptions.py
+++ xml4h/exceptions.py
@@ -3,14 +3,14 @@ Custom *xml4h* exceptions.
 """
 
 
-class BaseXml4hException(Exception):
+class Xml4hException(Exception):
     """
     Base exception class for all non-standard exceptions raised by *xml4h*.
     """
     pass
 
 
-class FeatureUnavailableException(BaseXml4hException):
+class FeatureUnavailableException(Xml4hException):
     """
     User has attempted to use a feature that is available in some *xml4h*
     implementations/adapters, but is not available in the current one.
@@ -18,7 +18,7 @@ class FeatureUnavailableException(BaseXml4hException):
     pass
 
 
-class IncorrectArgumentTypeException(ValueError, BaseXml4hException):
+class IncorrectArgumentTypeException(ValueError, Xml4hException):
     """
     Richer flavour of a ValueError that describes exactly what argument
     types are expected.
