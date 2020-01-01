aafb34224bc50ab0939ba9e844f7627402c55ec2
James Murty <james@murty.co>
2020-01-01 22:07:42 +1100

    Simplify Tox test-running commands and environments
    - move nose test-running commands out of setup.py to the Tox config
    - install and test against the lxml library by default, to avoid the
      need for with- and without-lxml environments for each Python version
    - run doc tests against every Python version, not just one and not
      in a custom extra environment
    - include a without-lxml test run with whatever is the default
      Python version, just to make sure that xml4h continues to work when
      that library isn't installed.

--- setup.py
+++ setup.py
@@ -1,26 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import sys
 import xml4h
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
-# Custom commands
-# TODO Worth making proper custom commands, per gist.github.com/2366194 ?
-if sys.argv[-1] == 'test-docs':
-    os.system(r'nosetests --verbose --nocapture'
-              r' --include=docs --ignore-files=\.py'
-              r' --with-doctest --doctest-extension=.rst')
-    sys.exit()
-elif sys.argv[-1] == 'test-coverage':
-    os.system(r'nosetests --nocapture'
-              r' --with-coverage --cover-package=xml4h')
-    sys.exit()
 
 setup(
     name=xml4h.__title__,
