7af21f45bd6ace13d84f5a7ebd24107eb4e53201
James Murty <james@murty.co>
2020-01-02 23:27:31 +1100

    Update copyright year in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'xml4h'
-copyright = '2012, James Murty'
+copyright = '2020, James Murty'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
